03cf4f2cc423a7ad6c04941ef57947a035e42a52
vhernandez <vhernandez@scienceleadership.org>
2019-11-06 00:46:00 -0500

    Initial commit

---
+++ src/org/sla/Main.java
@@ -0,0 +1,9 @@
+package org.sla;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("These are the top-selling albums of all time:");
+        Album.describeAlbums();
+    }
+}
